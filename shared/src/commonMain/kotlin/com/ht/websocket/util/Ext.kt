85d9eb94aeab726c9c84a9244cc65df19cd5ab20
Hafiz Taruligani <hafiztaruligani@gmail.com>
2025-07-10 05:29:52 +0700

    fix time format

--- shared/src/commonMain/kotlin/com/ht/websocket/util/Ext.kt
+++ shared/src/commonMain/kotlin/com/ht/websocket/util/Ext.kt
@@ -3,9 +3,14 @@ package com.ht.websocket.util
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
+import kotlin.math.min
 
 
 fun getCurrentTime(): String {
     val currentDateTime: LocalDateTime = kotlinx.datetime.Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
-    return "${currentDateTime.hour}.${currentDateTime.minute}"
+    var hour = currentDateTime.hour.toString()
+    if (hour.length<=1) hour = "0$hour"
+    var minutes = currentDateTime.minute.toString()
+    if (minutes.length<=1) minutes = "0${minutes}"
+    return "${hour}.${minutes}"
 }
